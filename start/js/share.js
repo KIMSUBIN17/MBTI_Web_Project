50c6ecdfae4d73e4dfe489463dc44a5afd8bd677
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2021-10-06 16:04:16 +0900

    add shareResult

---
+++ start/js/share.js
@@ -0,0 +1,36 @@
+function kakaoShare(){
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+        title: '오늘의 디저트',
+        description: '아메리카노, 빵, 케익',
+        imageUrl:
+            'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            androidExecutionParams: 'test',
+            },
+        },
+        
+        social: {
+            likeCount: 10,
+            commentCount: 20,
+            sharedCount: 30,
+        },
+        buttons: [
+        {
+            title: '웹으로 이동',
+            link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            },
+        },
+        {
+            title: '앱으로 이동',
+            link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            },
+        },
+    ]
+    });
+
+}    
